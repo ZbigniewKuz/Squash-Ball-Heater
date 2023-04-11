82b4504b3732a9452ff727bd6ff4e75afa89e2a2
ZbigniewKuz <120573209+ZbigniewKuz@users.noreply.github.com>
2023-04-11 22:05:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+// zadeklarowanie czujnika temperatury
+DHT dht(26, DHT22);
+
+//zadeklarowanie czujników przerwania i krańcówki
+const int przerwanie = 4;
+const int krancowka = 5;
+const int zalaczenieZasilaniaNaGrzalke = 6;
+
+//wartosci czujnikow
+int przerwanieStan = 0;
+int krancowkaStan = 0;
+
+//zadana temperatura przez uzytkownika
+float temperatura = 40.5;
+
+void setup() {
+  // put your setup code here, to run once:
+  dht.begin();
+  delay(2000);
+
+  pinMode(przerwanie, INPUT);
+  pinMode(krancowka, INPUT);
+  pinMode(zalaczenieZasilaniaNaGrzalke, OUTPUT);
+
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float temp = dht.readTemperature();
+  przerwanieStan = digitalRead(przerwanie);
+  krancowkaStan = digitalRead(krancowka);
+  //sprawdzamy czy temperatura jest w zakresie gdzie musimy grzac
+  //sprawdzamy czy czujniki sa zamkniete
+  if(przerwanieStan == HIGH && krancowkaStan == HIGH){
+  
+    if((temp > temperatura+2) || (temp <= temperatura+2)){
+
+      digitalWrite(zalaczenieZasilaniaNaGrzalke, HIGH);
+    }
+    else{
+
+      digitalWrite(zalaczenieZasilaniaNaGrzalke, LOW);
+
+    }
+  }
+
+}
